9b1021a7b73c15ea7054bc7d428db6984291600a
Garrett <gflynn@usc.edu>
2022-08-01 07:50:37 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,13 @@
 import anywave, sys
 anywave.init(sys.argv)
 print('Starting AnyWave HED plugin!')
-print(anywave.properties)  # output the content of a dict containing the global settings set by AnyWave for your plugin
 
-# anywave.debug_connect(59000)  # note that this code can be run from anywhere you want in your Python dev environment.
-# print('debug connected!')
+# show anywave properties
+properties = anywave.get_props()
+print('Properties', properties)
 
 # assign valid tags from the HED-SCORE library
-scoreTags = ['seizure', 'EI' ]
+scoreTags = ['seizure', 'EI' ] # TODO: Replace this with the actual list
 cfg = {
     'labels': scoreTags
 }
@@ -23,7 +23,5 @@ else:
     print('no markers with the following labels:', scoreTags)
     markers = anywave.get_markers()
     print('showing all markers', markers)
-
-
 
 # TODO: Add these markers to the BIDS dataset
